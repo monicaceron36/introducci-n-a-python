e845c9c1fe4f558277711c5d4a9d67651f23fc86
unknown <monica.ceron@correounivalle.edu.co>
2025-08-29 13:34:42 -0500

    solucion de ejercicio1.4

---
+++ operadores/operadores.py
@@ -0,0 +1,56 @@
+'''
+operadores matematicos
+suma
+resta 
+division
+multiplicacion
+potencia
+floor division
+modulo
+'''
+#operador suma
+x=2
+y=5
+print(x+y)
+w=2.5
+print (type(x), type(y))
+z= x+w
+print (z, type(z))
+
+#operador resta
+a=10
+b=15
+c=b-a
+print("el resultado de la resta es:",c, type(c))
+
+#operador division
+d=12
+f=4
+print(d/f)
+#operador mutiplicacion 
+a=50
+print(2*a)
+#potencia 
+Y=a**f
+print(f"el nuemero {a} elevado a la variable f{f} es {y}")
+#division piso 
+m=50
+n=3 
+print(m//n) 
+#modulo (%)
+g=10
+h=3
+print(g%h)   
+
+
+# es igual a
+print(2==2)
+print(2==2.0)
+
+# es diferente !=
+print("univalle"!="univalle")
+x = 4.123456789000001058946546
+y = 4.123456789000001
+
+      
+
